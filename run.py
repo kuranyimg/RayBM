454e3ace48e752547638330382f298ca9d531be3
kuranyimg <kuranyimg@gmail.com>
2024-10-31 08:24:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ class WebServer():
 
 
 class RunBot():
-  room_id = "670939cfd73a8630842fd3f5"
+  room_id = "6706b1dc20084804ef575ccb"
   bot_token = "52e5102011c184340d8a7bc34620bea190aa3fd840a62db9a7eaf58c06e636f9"
   bot_file = "main"
   bot_class = "Bot"
