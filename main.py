a66a3beca41eeb9650ad7bd2c0d3d165870dac34
kuranyimg <kuranyimg@gmail.com>
2025-05-03 20:30:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from highrise import*
 from highrise import BaseBot,Position
 from highrise.models import SessionMetadata
 from highrise import Highrise, GetMessagesRequest
+from highrise import BaseBot
+from functions.loop_emote import loop, stop_loop
 
 casa = ["I Marry You 💍","Of course I do 💍❤️","I don't want to 💍💔","Of course I don't 💍💔","I Love You Of course I marry you 💍"]
 
